9762142cd90efabaf3b423af40bfda4c5d3ac78e
Marc Eichenseher <eichenseher.marc@gmail.com>
2019-12-08 10:59:22 +0100

    Implement operation object
    Basically a object which helps us to determine which controller
    function is triggering. Based on this object we can build relevant
    paths (eg. templates).

--- src/Operations/Operation.php
+++ src/Operations/Operation.php
@@ -1,18 +1,45 @@
 <?php declare(strict_types=1); // -*- coding: utf-8 -*-
 
-namespace Ieim\LaravelControllerHelpers\Tests\Operations;
-
+namespace Ieim\LaravelControllerHelpers\Operations;
 
 use Ieim\LaravelContracts\Contracts\ControllerHelpers\Operations\OperationInterface;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
 
 class Operation implements OperationInterface
 {
+    const INVALID_CRUD_OPERATION_RECEIVED = 'Invalid crud operation received.';
+
+    private $operation;
+
+    /**
+     * @var Collection
+     */
+    private $operations;
+
+    /**
+     * Operation constructor.
+     *
+     * @param string $operation
+     */
+    public function __construct(string $operation, Collection $operations)
+    {
+        $this->operation = $operation;
+        $this->operations = $operations;
+    }
+
     /**
      * @inheritDoc
      */
     public static function fromCrudController(string $operation): OperationInterface
     {
-        return new Operation($operation);
+        $operations = collect(OperationInterface::OPERATIONS_CRUD);
+
+        if (! $operations->containsStrict($operation)) {
+            throw new InvalidArgumentException(self::INVALID_CRUD_OPERATION_RECEIVED);
+        }
+
+        return new Operation($operation, $operations);
     }
 
     /**
@@ -20,6 +47,22 @@ class Operation implements OperationInterface
      */
     public function current(): string
     {
-        // TODO: Implement current() method.
+        return $this->operation;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isValid(): bool
+    {
+        return $this->operations->containsStrict($this->operation);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function operations(): Collection
+    {
+        return $this->operations;
     }
 }
--- tests/Units/Operations/OperationTest.php
+++ tests/Units/Operations/OperationTest.php
@@ -3,47 +3,117 @@
 namespace Ieim\LaravelControllerHelpers\Tests\Operations;
 
 use Ieim\LaravelContracts\Contracts\ControllerHelpers\Operations\OperationInterface;
-use Ieim\LaravelContracts\Dummies\Contracts\ControllerHelpers\DummyCrud;
+use Ieim\LaravelControllerHelpers\Operations\Operation;
 use Ieim\LaravelControllerHelpers\Tests\BaseTestCase;
 
 class OperationTest extends BaseTestCase
 {
     public function operationProvider(): array
     {
+        $operationName = OperationInterface::OPERATION_INDEX;
+        $operations = collect(OperationInterface::OPERATIONS_CRUD);
+
         return [
-            [ new Operation() ],
+            [ new Operation($operationName, $operations), $operationName ],
         ];
     }
 
-    public function crudProvider(): array
+    public function falseyOperationProvider(): array
     {
+        $operationName = 'not_an_operation';
+        $operations = collect(OperationInterface::OPERATIONS_CRUD);
+
         return [
-            [ new DummyCrud() ],
+            [ new Operation($operationName, $operations), $operationName ],
         ];
     }
 
     /**
-     * @param DummyCrud $crud
-     * @dataProvider crudProvider
+     * @param Operation $operation
+     * @param string $operationName
+     * @dataProvider operationProvider
      */
-    public function testFromCrudController(DummyCrud $crud) :void
-    {
+    public function testFromCrudController(
+        Operation $operation,
+        string $operationName
+    ) :void {
+
         $expected = OperationInterface::class;
-        $actual = Operation::fromCrudController('', $crud);
+        $actual = Operation::fromCrudController($operationName);
 
         $this->assertInstanceOf($expected, $actual);
     }
 
     /**
      * @param Operation $operation
+     * @param string $operationName
+     * @dataProvider falseyOperationProvider
+     */
+    public function testFromCrudControllerFalseyOperation(
+        Operation $operation,
+        string $operationName
+    ) :void {
+
+        $this->expectErrorMessage(Operation::INVALID_CRUD_OPERATION_RECEIVED);
+        $actual = Operation::fromCrudController($operationName);
+    }
+
+    /**
+     * @param Operation $operation
+     * @param string $operationName
      * @dataProvider operationProvider
      */
-    public function testPath(
+    public function testCurrent(
+        Operation $operation,
+        string $operationName
+    ) : void {
+
+        $expected = $operationName;
+        $actual = $operation->current();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @param Operation $operation
+     * @param string $operationName
+     * @dataProvider operationProvider
+     */
+    public function testIsValidWhenItIs(
+        Operation $operation,
+        string $operationName
+    ) : void {
+
+        $actual = $operation->isValid();
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @param Operation $operation
+     * @param string $operationName
+     * @dataProvider falseyOperationProvider
+     */
+    public function testIsNotValidWhenItIsNot(
+        Operation $operation,
+        string $operationName
+    ) : void {
+
+        $actual = $operation->isValid();
+
+        $this->assertFalse($actual);
+    }
+
+    /**
+     * @param Operation $operation
+     * @dataProvider operationProvider
+     */
+    public function testOperations(
         Operation $operation
     ) : void {
 
-        $expected = 'dummy_current';
-        $actual = $operation->current();
+        $expected = collect(OperationInterface::OPERATIONS_CRUD);
+        $actual = $operation->operations();
 
         $this->assertEquals($expected, $actual);
     }
